fef20d22946da99b97c8bacae5e43b54dd7b65cd
tongyu <tongyu@tongyus-MacBook-Pro.local>
2020-12-27 17:38:45 -0600

    added port api

--- src/Router/router.js
+++ src/Router/router.js
@@ -3,7 +3,7 @@ import { getHot100 } from "../Controller/BillboardController.js";
 
 const router = express.Router();
 router.get("/hot100", getHot100);
-router.get("/port", () => {
-  return process.env.PORT;
+router.get("/port", (req, res) => {
+  res.send(process.env.PORT);
 });
 export default router;
