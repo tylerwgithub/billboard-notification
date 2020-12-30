63aee9be936adbaa9b34f025c70c71e4eb3df6f3
tongyu <tongyu@tongyus-MacBook-Pro.local>
2020-12-30 15:56:16 -0600

    add domain

--- src/Service/EmailService.js
+++ src/Service/EmailService.js
@@ -4,14 +4,14 @@ if (!transporter) {
   transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "tongyutest@gmail.com",
-      pass: "20201226",
+      user: "apakeetech@gmail.com",
+      pass: "20201230",
     },
   });
 }
 export const sendEmail = async (recipients, subject, email) => {
   return await transporter.sendMail({
-    from: '"Tongyu Tech" <tongyutest@gmail.com>', // sender address
+    from: '"Apakee" <apakeetech@gmail.com>', // sender address
     to: recipients ? recipients : "tyelsr@gmail.com", // list of receivers
     subject: subject, // Subject line
     html: email, // html body
