8d61ad9304bb4bf427c9f2d6790d3e34fe0f5bf7
tongyu <tongyu@tongyus-MacBook-Pro.local>
2020-12-29 23:31:10 -0600

    quick fix style

--- src/Service/BillboardService.js
+++ src/Service/BillboardService.js
@@ -5,11 +5,18 @@ export const getHot100Service = async (date) => {
   const thisWeek = chart.week;
   let email = chart.songs
     .map((song) => {
-      return song.rank + ": " + song.title + " - " + song.artist;
+      return (
+        "<tr><td>" +
+        song.rank +
+        ": " +
+        song.title +
+        " - " +
+        song.artist +
+        "</td></tr>"
+      );
     })
     .reduce((acc, cur) => {
-      const element = `<tr><td>${cur}</td></tr>`;
-      return acc + element;
+      return acc + cur;
     });
   email = `<h2>${"Week of " + chart.week}</h2><table>${email}</table>`;
   return { email, thisWeek };
